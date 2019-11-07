a5a29288b8d436d5e88d025321212950acfa6650
singhbalmeet <55771236+singhbalmeet@users.noreply.github.com>
2019-11-07 01:08:15 +0000

    Training Score: 27643.150071048076

--- KaggleGroup_Income.py
+++ KaggleGroup_Income.py
@@ -27,11 +27,12 @@ trainCol = pd.read_csv('tcd-ml-1920-group-income-train.csv')
 x_mod = trainCol
 
 
-print(x_mod.shape)
-print(y_mod.shape)
+#print(x_mod.shape)
+#print(y_mod.shape)
 
 x_mod['Yearly Income in addition to Salary (e.g. Rental Income)'] = x_mod['Yearly Income in addition to Salary (e.g. Rental Income)'].str.replace(' EUR', '')
 x_mod['Work Experience in Current Job [years]'] = x_mod['Work Experience in Current Job [years]'].str.replace('#NUM!', '0')
+x_mod['Housing Situation'] = x_mod['Housing Situation'].str.replace('0', 'nA')
 # Graph Plot
 #X = x_mod.iloc[:,0]
 #Y = x_mod.iloc[0:,-1]
@@ -49,7 +50,7 @@ x_mod = x_mod.dropna(subset = ['Year of Record', 'Work Experience in Current Job
 x_mod['Year of Record'].value_counts()
 # Dealing with Categorial Data
 # Dropping Max Option Columns
-x_mod = x_mod.drop(['Country', 'Profession', 'Size of City', 'Yearly Income in addition to Salary (e.g. Rental Income)' ], axis = 1)
+x_mod = x_mod.drop(['Instance', 'Country', 'Profession','Work Experience in Current Job [years]'], axis = 1)
 y_mod = x_mod.iloc[:,-1]
 x_mod = x_mod.drop(columns = 'Total Yearly Income [EUR]')
 # Get Dummies
@@ -86,7 +87,7 @@ x_mod = x_mod.drop(['Hair Color'],axis = 1)
 #x_mod = x_mod.drop(columns = 'Total Yearly Income [EUR]')
 
 #x_mod = x_mod.iloc[1:,:]
-x_train, x_test, y_train, y_test = train_test_split(x_mod, y_mod, test_size = 0.3)
+x_train, x_t, y_train, y_t = train_test_split(x_mod, y_mod, test_size = 0.3)
 
 
 model = LinearRegression()
@@ -94,7 +95,72 @@ print(y_train)
 print(y_train.shape)
 model.fit(x_train, y_train)
 
-pred = model.predict(x_test)
+pred = model.predict(x_t)
 
-print(mae(pred, y_test))
+print(mae(pred, y_t))
+# 32896.30037442671
+# 27643.150071048076
 
+
+testCol = pd.read_csv('tcd-ml-1920-group-income-test.csv')
+
+x_test = testCol
+
+x_test['Yearly Income in addition to Salary (e.g. Rental Income)'] = x_test['Yearly Income in addition to Salary (e.g. Rental Income)'].str.replace(' EUR', '')
+x_test['Work Experience in Current Job [years]'] = x_test['Work Experience in Current Job [years]'].str.replace('#NUM!', ' ')
+x_test['Housing Situation'] = x_test['Housing Situation'].str.replace('0', 'nA')
+      
+      
+x_test['Year of Record'].fillna(method = 'ffill', inplace = True)
+x_test['Size of City'].fillna(method = 'ffill', inplace = True)
+x_test['Yearly Income in addition to Salary (e.g. Rental Income)'].fillna(method = 'ffill', inplace = True)
+#x_test['Work Experience in Current Job [years]'].fillna(method = 'ffill', inplace = True)
+x_test['Satisfation with employer'].fillna(method = 'ffill', inplace = True)
+x_test['Gender'].fillna(method = 'ffill', inplace = True)
+x_test['Country'].fillna(method = 'ffill', inplace = True)
+x_test['University Degree'].fillna(method = 'ffill', inplace = True)
+x_test['Hair Color'].fillna(method = 'ffill', inplace = True)
+x_test['Profession'].fillna(method = 'ffill', inplace = True)
+
+x_test = x_test.drop(['Instance', 'Country', 'Profession','Total Yearly Income [EUR]','Work Experience in Current Job [years]'], axis = 1)
+
+
+# Get Dummies
+dummy = pd.get_dummies(x_test['Housing Situation'])
+x_test = pd.concat([x_test,dummy],axis = 1)
+x_test = x_test.drop(['Housing Situation'],axis = 1) 
+
+dummy = pd.get_dummies(x_test['Satisfation with employer'])
+x_test = pd.concat([x_test,dummy],axis = 1)
+x_test = x_test.drop(['Satisfation with employer'],axis = 1)
+
+dummy = pd.get_dummies(x_test['Gender'])
+x_test = pd.concat([x_test,dummy],axis = 1)
+x_test = x_test.drop(['Gender'],axis = 1)
+
+dummy = pd.get_dummies(x_test['University Degree'])
+x_test = pd.concat([x_test,dummy],axis = 1)
+x_test = x_test.drop(['University Degree'],axis = 1)
+
+dummy = pd.get_dummies(x_test['Hair Color'])
+x_test = pd.concat([x_test,dummy],axis = 1)
+x_test = x_test.drop(['Hair Color'],axis = 1)
+
+x_test.info()
+x_mod.info()
+
+x_test.columns
+x_mod.columns
+#x_test.isnull().sum()
+
+model2 = LinearRegression()
+
+model2.fit(x_mod, y_mod)
+
+pred2 = model2.predict(x_test)
+
+print(pred2)
+with open('output', 'w') as out:
+    for i in pred2:
+        out.write(str(i))
+        out.write('\n')
